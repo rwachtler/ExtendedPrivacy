ba83c327ab3d72ee4382a6518c43597d5a768024
Ramis <ramis.wachtler@outlook.com>
2018-02-25 14:16:06 +0100

    Implemented tracking iframe infor retrieval
    - Plugin is now visible in navigation
    - Adapted template
    - Added some translations
    - Added ability to retrieve tracking-frame info

--- Controller.php
+++ Controller.php
@@ -8,6 +8,12 @@
 
 namespace Piwik\Plugins\ExtendedPrivacy;
 
+use Piwik\Common;
+use Piwik\Plugins\LanguagesManager\LanguagesManager;
+use Piwik\Piwik;
+use Piwik\Site;
+use Piwik\Url;
+use Piwik\UrlHelper;
 use Piwik\View;
 
 /**
@@ -18,11 +24,61 @@ use Piwik\View;
  */
 class Controller extends \Piwik\Plugin\Controller
 {
-    public function index()
-    {
-        // Render the Twig template templates/index.twig and assign the view variable answerToLife to the view.
-        return $this->renderTemplate('index', array(
-            'answerToLife' => 42
-        ));
+    public function extendedPrivacySettings() {
+        Piwik::checkUserHasSomeAdminAccess();
+        if (Piwik::hasUserSuperUserAccess()) {
+            $trackingInfo = $this->getTrackingInfoIframe();
+            $viewData = array(
+                'transparencyType' => isset($trackingInfo['type']) ? $trackingInfo['type'] : '',
+                'transparencyIframeContent' => isset($trackingInfo['content']) ? $trackingInfo['content'] : ''
+            );
+        }
+        return $this->renderTemplate('index', $viewData);
+    }
+
+    /**
+     * Retrieves information from tracking iframes
+     */
+    protected function getTrackingInfoIframe() {
+        $UrlInstance = new Url();
+        $query = $UrlInstance->getArrayFromCurrentQueryString();
+        $SiteInstance = new Site($query['idSite']);
+        $mainUrl = $SiteInstance->getMainUrl();
+        $possibleSubpages = array(
+            '',
+            Piwik::translate('ExtendedPrivacy_UrlPrivacy'),
+            Piwik::translate('ExtendedPrivacy_UrlPrivacyAlt'),
+            Piwik::translate('ExtendedPrivacy_UrlTerms'),
+            Piwik::translate('ExtendedPrivacy_UrlLegal')
+        );
+
+        $iframesArr = array();
+        libxml_use_internal_errors(true);
+        foreach ($possibleSubpages as $subpage) {
+            $DOMInstance = new \DOMDocument();
+            $requestUrl = $mainUrl . '/' . $subpage;
+            $urlValidity = get_headers($requestUrl , 1);
+            if (strpos($urlValidity[0], '200 OK')) {
+                $DOMInstance->loadHTMLFile($requestUrl);
+                $iframeElementsForSubpage = $DOMInstance->getElementsByTagName('iframe');
+                if ($iframeElementsForSubpage->length > 0) {
+                    $iframesArr[] = $iframeElementsForSubpage;
+                }
+            }
+        }
+        foreach ($iframesArr as $nodeList) {
+            foreach ($nodeList as $iframeNode) {
+                $iframeTargetUrl = $iframeNode->attributes[1]->value;
+                if (strpos($iframeTargetUrl, 'module=CoreAdminHome&action=optOut')) {
+                    $DOMInstance->loadHTMLFile($iframeTargetUrl);
+                    $content = $DOMInstance->getElementsByTagName('body');
+                    return array(
+                        'type' => 'default',
+                        'content' => $content[0]->nodeValue
+                    );
+                }
+            }
+        }
+        return array();
     }
 }
--- Menu.php
+++ Menu.php
@@ -9,7 +9,7 @@
 namespace Piwik\Plugins\ExtendedPrivacy;
 
 use Piwik\Menu\MenuAdmin;
-use Piwik\Menu\MenuTop;
+use Piwik\Piwik;
 
 /**
  * This class allows you to add, remove or rename menu items.
@@ -18,19 +18,12 @@ use Piwik\Menu\MenuTop;
  */
 class Menu extends \Piwik\Plugin\Menu
 {
-
-    public function configureTopMenu(MenuTop $menu)
-    {
-        // $menu->addItem('My Top Item', null, $this->urlForDefaultAction(), $orderId = 30);
-    }
-
     public function configureAdminMenu(MenuAdmin $menu)
     {
-        // reuse an existing category. Execute the showList() method within the controller when menu item was clicked
-        // $menu->addManageItem('My User Item', $this->urlForAction('showList'), $orderId = 30);
-        // $menu->addPlatformItem('My User Item', $this->urlForDefaultAction(), $orderId = 30);
-
-        // or create a custom category
-        // $menu->addItem('CoreAdminHome_MenuManage', 'My User Item', $this->urlForDefaultAction(), $orderId = 30);
+        if (Piwik::isUserHasSomeAdminAccess()) {
+            $menu->addSystemItem('ExtendedPrivacy_MenuExtendedPrivacySettings',
+                                 $this->urlForAction('extendedPrivacySettings'),
+                                 $order = 26);
+        }
     }
 }
