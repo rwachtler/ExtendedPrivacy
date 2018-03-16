6c61e27ec3dc20f6637b4c87c679094a7ed87d62
Ramis <ramis.wachtler@outlook.com>
2018-03-16 22:04:20 +0100

    Commented out the transparency iframe content

--- Controller.php
+++ Controller.php
@@ -39,7 +39,7 @@ class Controller extends \Piwik\Plugin\Controller
                 Piwik::translate('CoreAdminHome_ClickHereToOptOut');
             $viewData = array(
                 'transparencyType' => isset($trackingInfo['type']) ? $trackingInfo['type'] : '',
-                'transparencyIframeContent' => isset($trackingInfo['content']) ? $defaultOptOutTranslation : '',
+                // 'transparencyIframeContent' => isset($trackingInfo['content']) ? $defaultOptOutTranslation : '',
                 'anonymizeIPInUse' => $anonymizeIPInfo['enabled'],
                 'anonymizeIPMaskLength' => $anonymizeIPInfo['maskLength'],
                 'anonymizeIPForAnonymousVisitEnrichment' => $anonymizeIPInfo['useAnonymizedIpForVisitEnrichment'],
